dace9d21306a13c036145d0060dec99cbe8f3c59
whuuarno7 <ju.davwuarno@gmail.com>
2015-03-20 01:22:11 -0500

    validaciones de eventos out

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
-	validates :name, :description, presence: true
-	validates :name,  {danza, teatro, música, deporte, arquitectura, museo, exposición, adultos, niños, biblioteca, cine, lectura, arte, academia, }
+	#validates :name, :description, presence: true
+	#validates :name,  {danza, teatro, música, deporte, arquitectura, museo, exposición, adultos, niños, biblioteca, cine, lectura, arte, academia, }
 
 	#f.select :name, options_for_select([])
 
