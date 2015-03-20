8c07c5560c8b8c90ab4f2e8ec89969184e4b7252
whuuarno7 <ju.davwuarno@gmail.com>
2015-03-20 00:51:09 -0500

    quito users for de routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,6 @@ end
   devise_for :users
 
 
-devise_for :users, :controllers => { registrations: 'registrations' }
 
 
   # The priority is based upon order of creation: first created -> highest priority.
