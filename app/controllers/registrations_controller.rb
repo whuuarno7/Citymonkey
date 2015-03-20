c23bea905236d80373d160fa0f520001bb42343a
whuuarno7 <ju.davwuarno@gmail.com>
2015-03-20 00:28:08 -0500

    registrations controller modificado

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,16 +3,19 @@ class Users::RegistrationsController < Devise::RegistrationsController
  before_action :authenticate_user!
 
   def profile_user 
-	@user =  User.find(params[:id])
+	@user=User.find(params[:id])
 	end 	 
     
-end
 
+ private
  def update_resource
     devise_parameter_sanitizer.sanitize(:name, :sex, :tel, :bibliography, :locationborn, :website, :borndate, :avatar_file_name, :backgorund_file_name)
+  
   end
    
   def account_update_params
     params.require(:user).permit(:name, :sex, :tel, :bibliography, :locationborn, :website, :borndate, :avatar_file_name, :backgorund_file_name)
+  
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,9 @@ end
   devise_for :users
 
 
+devise_for :users, :controllers => { registrations: 'registrations' }
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
